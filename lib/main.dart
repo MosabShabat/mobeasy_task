3bb1b8764547a36f6e43c43be84c4f8c3edca6e1
MosabShabat <mosabshabat39@gmail.com>
2023-09-25 11:49:38 +0300

    aaaa

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobeasy_task/bloc/auth/auth_bloc.dart';
+import 'package:mobeasy_task/data/repositories/auth_repository.dart';
+import 'package:mobeasy_task/presentation/auth/sign_in/sign_in.dart';
+import 'package:mobeasy_task/presentation/dashboard/dashboard.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: BlocProvider(
+        create: (context) => AuthBloc(
+          authRepository: RepositoryProvider.of<AuthRepository>(context),
+        ),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              // primarySwatch: Colors.green,
+              ),
+          home: StreamBuilder<User?>(
+              // we will user userChanges() instead to authStateChanges() to rebuild drawer with latest profile image
+              stream: FirebaseAuth.instance.userChanges(),
+              builder: (context, snapshot) {
+                // If the snapshot has user data, then they're already signed in.
+                if (snapshot.hasData) {
+                  return Dashboard(snapshot.data!);
+                }
+                // Otherwise, they're not signed in
+                return const SignIn();
+              }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/auth/sign_in/sign_in.dart
@@ -0,0 +1,105 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:mobeasy_task/bloc/auth/auth_bloc.dart';
+import 'package:mobeasy_task/presentation/dashboard/dashboard.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is Authenticated) {
+            // Navigating to the dashboard screen if the user is authenticated
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        Dashboard(FirebaseAuth.instance.currentUser!)));
+          }
+          if (state is AuthError) {
+            // Showing the error message if the user has entered invalid credentials
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.error)));
+          }
+        },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is Loading) {
+              // Showing the loading indicator while the user is signing in
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is UnAuthenticated) {
+              // Showing the sign in form if the user is not authenticated
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: SingleChildScrollView(
+                    reverse: true,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Welcome back, Sign In",
+                          style: TextStyle(
+                            fontSize: 38,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        SignInButton(
+                          Buttons.Google,
+                          text: "Sign in with Google",
+                          onPressed: () {
+                            _authenticateWithGoogle(context);
+                          },
+                        )
+                            .box
+                            .width(context.screenWidth)
+                            .height(context.screenHeight / 16)
+                            // .color(Colors.green)
+                            .make(),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _authenticateWithGoogle(context) {
+    BlocProvider.of<AuthBloc>(context).add(
+      GoogleSignInRequested(),
+    );
+  }
+}
